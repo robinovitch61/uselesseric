9293ae08fc28496fa6f7202b7cfeee9a40fe09bb
Leo Robinovitch <leorobinovitch@gmail.com>
2021-12-10 15:17:57 -0800

    so useless <3

---
+++ interactive.js
@@ -0,0 +1,43 @@
+const numImages = 8
+
+const imagePath = "public/img/"
+
+function generateImagePaths(prefix) {
+  const paths = []
+  for (let i = 1; i <= numImages; i++) {
+    paths.push(imagePath + `${prefix}-${('0' + i).slice(-2)}.png`)
+  }
+  return paths
+}
+
+const topImages = generateImagePaths("top")
+const midImages = generateImagePaths("mid")
+const lowImages = generateImagePaths("low")
+
+function setup(prefix, images) {
+  const image = document.getElementById(`${prefix}-img`)
+  let idx = 0
+  image.src = images[idx]
+  const backButton = document.getElementById(`${prefix}-back`)
+  backButton.onclick = () => {
+    if (idx === 0) {
+      idx = images.length - 1
+    } else {
+      idx--
+    }
+    image.src = images[idx]
+  }
+  const nextButton = document.getElementById(`${prefix}-next`)
+  nextButton.onclick = () => {
+    if (idx === images.length - 1) {
+      idx = 0
+    } else {
+      idx++
+    }
+    image.src = images[idx]
+  }
+}
+
+setup("top", topImages)
+setup("mid", midImages)
+setup("low", lowImages)
